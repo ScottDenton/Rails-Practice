cc26a9147887d933728cceb5a2241e638cce4ea9
ScottDenton <scottyd_14@hotmail.com>
2019-05-29 17:50:31 -0700

    new posting form and functionality added

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -6,4 +6,19 @@ class PostingsController < ApplicationController
   def show
     @posting = Posting.find(params[:id])
   end
+
+  def new
+    @companies = Company.all
+  end
+
+  def create
+    Posting.create(posting_params)
+    redirect_to postings_path
+  end
+
+  private
+
+  def posting_params
+    params.permit(:position, :location, :job_description, :company_id)
+  end
 end
